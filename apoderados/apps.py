0f13aaa4fada95d261260b7d039273824ff00eea
Nevado <axel.erico.leon.garcia@gmail.com>
2025-10-06 17:01:58 -0500

    Modulos/apps creados en Django

--- apoderados/apps.py
+++ apoderados/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CeamadjConfig(AppConfig):
+class ApoderadosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CEAMADJ'
+    name = 'apoderados'
