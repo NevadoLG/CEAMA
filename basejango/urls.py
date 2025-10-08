bd77c250d224752e9b95b73bc58c80f1f2887108
Nevado <axel.erico.leon.garcia@gmail.com>
2025-10-08 14:35:07 -0500

    Pagina virtual

--- basejango/urls.py
+++ basejango/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('estudiantes/', include('estudiantes.urls')),
-
+    path('', include('landing.urls')),
 ]
