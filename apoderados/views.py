1596e409b65fc1bbe4a7cb47d25f20e7c6f5ad76
afefo <alfreditooouuu@gmail.com>
2025-10-08 20:37:19 -0500

    CSS de vista registro estudiante

--- apoderados/views.py
+++ apoderados/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Apoderado
 
-# Create your views here.
+def registrar_apoderado(request):
+    if request.method == 'POST':
+        nombres = request.POST.get('nombres')
+        apellidos = request.POST.get('apellidos')
+        telefono = request.POST.get('telefono')
+        correo = request.POST.get('correo')
+        direccion = request.POST.get('direccion')
+
+        # Validar que no exista el teléfono
+        if Apoderado.objects.filter(telefono=telefono).exists():
+            messages.error(request, "Ya existe un apoderado con ese número de teléfono.")
+        else:
+            Apoderado.objects.create(
+                nombres=nombres,
+                apellidos=apellidos,
+                telefono=telefono,
+                correo=correo,
+                direccion=direccion
+            )
+            messages.success(request, "Apoderado registrado exitosamente.")
+            return redirect('registrar_apoderado')
+
+    apoderados = Apoderado.objects.all().order_by('-id')
+    return render(request, 'panel/registrar_apoderado.html', {'apoderados': apoderados})
